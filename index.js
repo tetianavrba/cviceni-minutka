2bd0f29fc8fd5088f6e5c27ffe90128166a8104a
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-10-13 20:08:09 +0200

    JS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+const zazvon = () => {
+  const alarmElement = document.querySelector('.alarm');
+  alarmElement.classList.add('alarm--ring');
+  const audioElement = document.querySelector('audio');
+  audioElement.play();
+};
+
+const sekundy = Number(
+  prompt('Kolik vteřin má uplynout než minutka začne zvonit?'),
+);
+
+const vysledek = setTimeout(zazvon, sekundy * 1000);
+
+const buttonElement = document.querySelector('button');
+buttonElement.addEventListener('click', () => {
+  clearTimeout(vysledek);
+});
